9898b29ed807aa492240be93dfe1a50f041e30fb
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2017-04-05 21:40:31 +0100

    Separate gulp CSS build tasks to dev and pub

--- src/nab/gulpfile.js
+++ src/nab/gulpfile.js
@@ -2,19 +2,19 @@
 var loadJsonFile = require('load-json-file');
 var sass = require('gulp-sass');
 var sassLint = require('gulp-sass-lint');
-var concat = require('gulp-concat');
+//var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
+var cleanCss = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
 var config = loadJsonFile.sync('gulpconfig.json');
 var scssInput = config.paths.stylesheets.scss;
 var cssOutput = config.paths.stylesheets.css;
 
 var sassOptions = {
-    errLogToConsole: true,
-    outputStyle: 'expanded' //compressed
+    errLogToConsole: true
 };
 
-gulp.task('pub-css', () => {
+gulp.task('dev-css', () => {
     return gulp
         .src(scssInput)
         .pipe(sassLint())
@@ -22,18 +22,26 @@ gulp.task('pub-css', () => {
         .pipe(sassLint.failOnError())
         .pipe(sourcemaps.init())
         .pipe(sass(sassOptions).on('error', sass.logError))
-        .pipe(sourcemaps.write())
         .pipe(autoprefixer())
-        .pipe(concat('site.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(cssOutput));
+});
+
+gulp.task('pub-css', () => {
+    return gulp
+        .src(scssInput)
+        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(cleanCss())
         .pipe(gulp.dest(cssOutput));
 });
 
 gulp.task('watch', () => {
     return gulp
-        .watch(scssInput, ['pub-css'])
+        .watch(scssInput, ['dev-css'])
         .on('change', (event) => {
             console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
         });
 });
 
-gulp.task('default', ['pub-css', 'watch']);
+gulp.task('default', ['dev-css', 'watch']);
