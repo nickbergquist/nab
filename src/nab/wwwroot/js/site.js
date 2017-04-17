10f02d4e8356c505e9c6426d5b86ad3bdb170191
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2017-04-17 23:12:27 +0100

    Update to basic styling

--- src/nab/wwwroot/js/site.js
+++ src/nab/wwwroot/js/site.js
@@ -1,1 +1,12 @@
-﻿// Write your Javascript code.
+﻿// script loaded in all pages
+
+document.documentElement.className = 'js';
+
+// touch events support
+if (!('ontouchstart' in document.documentElement)) {
+    document.documentElement.className += ' no-touch';
+}
+
+$(function () {
+    //console.log('jQuery - DOM ready');
+});
