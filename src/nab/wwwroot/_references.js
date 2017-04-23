9193fb9b79c3af60f10a444dc35e46aae83e6f6d
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2017-04-23 23:39:56 +0100

    Add enquire media-queries library

--- src/nab/wwwroot/_references.js
+++ src/nab/wwwroot/_references.js
@@ -1,4 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
+/// <reference path="js/enquire.min.js" />
+/// <reference path="js/matchmedia.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
--- src/nab/wwwroot/js/site.js
+++ src/nab/wwwroot/js/site.js
@@ -1,6 +1,6 @@
 ﻿// script loaded in all pages
 
-document.documentElement.className = 'js';
+document.documentElement.className += ' js';
 
 // touch events support
 if (!('ontouchstart' in document.documentElement)) {
@@ -8,5 +8,42 @@ if (!('ontouchstart' in document.documentElement)) {
 }
 
 $(function () {
-    //console.log('jQuery - DOM ready');
+    enquire.register('screen and (max-width: 479px)', {
+        match: function () {
+            siteNavBehaviour.navCondensed();
+        }
+    }).register('screen and (min-width: 480px)', {
+        match: function () {
+            siteNavBehaviour.navExpanded();
+        }
+    }).listen(50); // milliseconds
 });
+
+
+// METHODS
+
+var siteNavBehaviour = {
+    navCondensed: function () {
+        if (!$('header .nav-button-show').length) {
+            var $header = $('header');
+            var $headerNav = $('header nav');
+            var $buttonNavVisibility = $('<button type="button" class="nav-button-show">Show/Hide</button>');
+
+            $buttonNavVisibility.prependTo($header);
+
+            $buttonNavVisibility.click(function () {
+                if ($headerNav.hasClass('show')) {
+                    $headerNav.removeClass('show');
+                } else {
+                    $headerNav.addClass('show');
+                }
+            });
+        }
+    },
+    navExpanded: function () {
+        $('header nav').removeClass('show');
+        if ($('header .nav-button-show').length) {
+            $('header .nav-button-show').remove();
+        }
+    }
+};
