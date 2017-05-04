5cec1d058302a5761cdc4151dbc3e05a046a8a63
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2017-05-05 00:22:08 +0100

    Add contact form basic styles and HTML

--- src/nab/wwwroot/js/site.js
+++ src/nab/wwwroot/js/site.js
@@ -17,6 +17,10 @@ $(function () {
             siteNavBehaviour.navExpanded();
         }
     }).listen(50); // milliseconds
+
+    if ($('.page-contact').length) {
+        $('#form-contact-name').focus();
+    }
 });
 
 
@@ -24,52 +28,54 @@ $(function () {
 
 var siteNavBehaviour = {
     navCondensed: function () {
-        var $headerNav = $('#nav-site');
-        var $buttonNavVisibility = $('<button type="button" class="nav-button-show" aria-expanded="false" aria-controls="nav-site"><span>Show/Hide</span></button>');
+        if (!$('header .nav-button-show').length) {
+            var $headerNav = $('#nav-site');
+            var $buttonNavVisibility = $('<button type="button" class="nav-button-show" aria-expanded="false" aria-controls="nav-site"><span>Show/Hide</span></button>');
 
-        $headerNav.find('a').attr('tabindex', -1);
+            $headerNav.find('a').attr('tabindex', -1);
 
-        $buttonNavVisibility.on('click', function () {
-            if (!$headerNav.hasClass('active')) {
-                $headerNav.find('a').attr('tabindex', 0);
-                $headerNav.addClass('active');
-                $(this).addClass('active').attr('aria-expanded', 'true');
-            } else {
-                $headerNav.find('a').attr('tabindex', -1);
-                $headerNav.removeClass('active');
-                $(this).removeClass('active').attr('aria-expanded', 'false');
-            }
-        });
-            
-        $headerNav.find('a').last().keydown(function (e) {
-            if (e.shiftKey && e.keyCode == 9) { // 'Shift + Tab' : navigation backwards
-                return; // don't close the <nav>
-            } else if (e.keyCode == 9) { // 'Tab' forwards only
-                $($buttonNavVisibility).click();
-            }
-        });
+            $buttonNavVisibility.on('click', function () {
+                if (!$headerNav.hasClass('active')) {
+                    $headerNav.find('a').attr('tabindex', 0);
+                    $headerNav.addClass('active');
+                    $(this).addClass('active').attr('aria-expanded', 'true');
+                } else {
+                    $headerNav.find('a').attr('tabindex', -1);
+                    $headerNav.removeClass('active');
+                    $(this).removeClass('active').attr('aria-expanded', 'false');
+                }
+            });
 
-        // close the <nav> if navigation backwards beyond the control
-        $buttonNavVisibility.on('keydown', function (e) {
-            if (e.shiftKey && e.keyCode == 9) { // 'Shift + Tab'
+            $headerNav.find('a').last().keydown(function (e) {
+                if (e.shiftKey && e.keyCode == 9) { // 'Shift + Tab' : navigation backwards
+                    return; // don't close the <nav>
+                } else if (e.keyCode == 9) { // 'Tab' forwards only
+                    $($buttonNavVisibility).click();
+                }
+            });
+
+            // close the <nav> if navigation backwards beyond the control
+            $buttonNavVisibility.on('keydown', function (e) {
+                if (e.shiftKey && e.keyCode == 9) { // 'Shift + Tab'
+                    if ($headerNav.hasClass('active')) {
+                        $($buttonNavVisibility).click();
+                    }
+                }
+            });
+
+            // close the <nav> if user input made elsewhere in the page 
+            $('html').on('click', function (e) {
                 if ($headerNav.hasClass('active')) {
-                    $($buttonNavVisibility).click();
+                    var $target = $(e.target); //console.log('click', e.target);
+
+                    if ((!$target.is($buttonNavVisibility)) && ($target.parents('#nav-site').length == 0)) {
+                        $($buttonNavVisibility).click();
+                    }
                 }
-            }
-        });
+            });
 
-        // close the <nav> if user input made elsewhere in the page 
-        $('html').on('click', function (e) {
-            if ($headerNav.hasClass('active')) {
-                var $target = $(e.target); //console.log('click', e.target);
-
-                if ((!$target.is($buttonNavVisibility)) && ($target.parents('#nav-site').length == 0)) {
-                    $($buttonNavVisibility).click();
-                }
-            }
-        });
-
-        $headerNav.before($buttonNavVisibility);
+            $headerNav.before($buttonNavVisibility);
+        }
     },
     navExpanded: function () {
         $('#nav-site').removeClass('active');
